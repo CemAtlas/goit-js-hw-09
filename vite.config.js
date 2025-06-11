500c59405f46ca7973a166ae3fb0e79281ec9fc1
Cem-Atlas <cematlas00@gmail.com>
2025-06-11 17:46:44 +0300

    de

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,3 @@
-
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-export default defineConfig({
-  root: './src', 
+export default {
   base: './goit-js-hw-09',
-  publicDir: '../public', 
-  
-  build: {
-    outDir: '../dist', 
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.html'),
-        gallery: resolve(__dirname, 'src/01-gallery.html'),
-        form: resolve(__dirname, 'src/02-form.html')
-      }
-    }
-  },
-  
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
-  }
-});
+};
